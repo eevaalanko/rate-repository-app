048254cbba5316362ab8c9bce4014f0396829af6
Eeva Alanko <eeva.alanko@siili.com>
2020-11-05 15:59:30 +0200

    add exercise 10.15

--- App.js
+++ App.js
@@ -3,17 +3,21 @@ import { NativeRouter } from "react-router-native";
 import { ApolloProvider } from "@apollo/react-hooks";
 import Constants from "expo-constants";
 import Main from "./src/components/Main";
-
 import createApolloClient from "./src/utils/apolloClient";
+import AuthStorage from "./src/utils/authStorage";
+import AuthStorageContext from "./src/contexts/AuthStorageContext";
 
-const apolloClient = createApolloClient();
+const authStorage = new AuthStorage();
+const apolloClient = createApolloClient(authStorage);
 
 const App = () => {
   console.log(Constants.manifest);
   return (
     <NativeRouter>
       <ApolloProvider client={apolloClient}>
-        <Main />
+        <AuthStorageContext.Provider value={authStorage}>
+          <Main />
+        </AuthStorageContext.Provider>
       </ApolloProvider>
     </NativeRouter>
   );
--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,17 +1,22 @@
 import { SIGN_IN } from "../graphql/mutations";
 import AsyncStorage from '@react-native-community/async-storage';
-import {useMutation} from "@apollo/react-hooks";
+import {useMutation, useApolloClient} from "@apollo/react-hooks";
+import { useContext } from 'react';
+import AuthStorageContext from '../contexts/AuthStorageContext';
 
 export const useSignIn = () => {
+  const authStorage = useContext(AuthStorageContext);
   const [mutate, result] = useMutation(SIGN_IN);
+  const apolloClient = useApolloClient();
   const signIn = async ({ username, password }) => {
     const { data } = await mutate({
       variables: { credentials: { username, password } },
     });
     console.log('data: ', data)
     if(data){
-      const token = data.authorize.accessToken;
-      await AsyncStorage.setItem(token);
+      const token = await data.authorize.accessToken;
+      await authStorage.setAccessToken(token);
+      await apolloClient.resetStore();
     }
     return data
   };
